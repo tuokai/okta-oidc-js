c1d32711932d74eef55ed968a3a35405671823f4
Len Boyette <lboyette@okta.com>
2017-08-28 16:24:34 -0700

    add publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -vx
+
+source $OKTA_HOME/$REPO/scripts/setup.sh
+
+npm install -g lerna
+
+
+
+#npm config set registry "https://artifacts.aue1d.saasure.com/artifactory/api/npm/npm-okta"
+
+echo "hi there"
+#lerna publish
